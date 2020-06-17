65c3c6cd9ee5d6bc5b77dd2dee224afa6e059f59
Paul Miller <paul@paulmillr.com>
2020-06-17 14:57:30 +0300

    Release 0.4.1.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 type B58TYPE = 'ipfs' | 'flickr' | 'btc' | 'xrp' | 'xmr';
 export function encode(source: string|Uint8Array, type?: B58TYPE): string;
+export function decode(output: string, type?: B58TYPE): string;
 export default encode;
