556e228a5c2b933fe0550ab4bef59c64b1fd2bec
Matthew Little <zone117x@gmail.com>
2021-03-10 18:31:39 +0100

    fix: uncomment a few test vectors

--- test/test.ts
+++ test/test.ts
@@ -14,13 +14,13 @@ function asciiToArray(str: string) {
 
 const vectors1 = [
   {decoded: 'hello world', encoded: 'StV1DL6CwTryKyV'},
-  //{decoded: Buffer.from('hello world'), encoded: 'StV1DL6CwTryKyV'},
+  {decoded: Buffer.from('hello world'), encoded: 'StV1DL6CwTryKyV'},
   {decoded: asciiToArray('hello world'), encoded: 'StV1DL6CwTryKyV'},
   {decoded: 'hello world', encoded: 'StVrDLaUATiyKyV', isXRP: true},
   {decoded: '\0\0hello world', encoded: '11StV1DL6CwTryKyV'},
   {decoded: '', encoded: ''},
-  // {decoded: Buffer.from([0x51, 0x6b, 0x6f, 0xcd, 0x0f]), encoded: 'ABnLTmg'},
-  // {decoded: new Uint8Array([0x51, 0x6b, 0x6f, 0xcd, 0x0f]), encoded: 'ABnLTmg'},
+  {decoded: Buffer.from([0x51, 0x6b, 0x6f, 0xcd, 0x0f]), encoded: 'ABnLTmg'},
+  {decoded: new Uint8Array([0x51, 0x6b, 0x6f, 0xcd, 0x0f]), encoded: 'ABnLTmg'},
   {decoded: "Hello World!", encoded: "2NEpo7TZRRrLZSi2U"},
   {decoded: "The quick brown fox jumps over the lazy dog.", encoded: "USm3fpXnKG5EUBx2ndxBDMPVciP5hGey2Jh4NDv6gmeo1LkMeiKrLJUUBk6Z"},
   {decoded: new Uint8Array([0x00, 0x00, 0x28, 0x7f, 0xb4, 0xcd]), encoded: "11233QC4"}
@@ -37,7 +37,7 @@ test('vectors1', () => {
     expect(encoded).toBe(vector.encoded);
 
     const decoded = decode(encoded, alphabet || undefined);
-    expect(decoded).toEqual(vectorDecodedArr);
+    expect(decoded.buffer).toEqual(vectorDecodedArr.buffer);
   }
 });
 
